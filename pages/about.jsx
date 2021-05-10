8e89acd6306d63f05c43cb2a54bb7bd31a1272f1
Arthur <arthurd800@wp.pl>
2021-05-10 10:11:08 +0200

    initial commit

---
+++ pages/about.jsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Earum quaerat quod dignissimos architecto quidem modi totam
+                    odio accusantium, harum quos rem voluptas quae! Vel aliquam
+                    porro culpa velit nam vero!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae neque rem id quas ea tempora ducimus ratione
+                    adipisci accusantium dolorem eveniet officia eligendi a
+                    culpa sit alias deserunt tenetur, dicta ipsum modi
+                    repudiandae illum quod sed. Autem, quasi. Maxime dolor ipsum
+                    perferendis quas cum, quisquam officia doloremque
+                    reprehenderit numquam inventore nesciunt eaque veritatis, ex
+                    neque sapiente accusantium impedit rerum quaerat sint dolore
+                    magni quia. Asperiores, quaerat!
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default About;
